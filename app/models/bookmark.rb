55b0a5bc80fd15ef256d366af2b3f73da92388ad
benjamin boisson <benjbdk@gmail.com>
2021-11-15 09:37:55 +0100

    osdmtdt

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6, maximum: 20 }
+  validates :comment, length: { minimum: 6, maximum: 40 }
   validates :movie, uniqueness: { scope: :list }
 end
